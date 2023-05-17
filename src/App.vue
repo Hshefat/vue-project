779cbd442ec1e25bea989eaaa2b789024fd1428f
Hshefat <shefat.hasnain>
2023-05-17 18:52:34 +0600

    Add all my files

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
+    <router-link to="/sign-up">Sign Up</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,5 @@
+<template>
+    <h2>
+        /sign-up
+    </h2>
+</template>
